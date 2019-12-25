bbb3ba22e79a96c77422988bd9fd9a1b20bd05e5
Aji Nugraha Hidayat <tts2147@gmail.com>
2019-12-26 02:52:30 +0700

    restaurant H1

---
+++ app/src/main/java/com/restaurant/teditiansukma/data/Constans.java
@@ -0,0 +1,7 @@
+package com.restaurant.teditiansukma.data;
+
+public class Constans {
+    public static final String WEB_URL_RESTORAN = "http://www.pakartanaman.com/skripsi/public/";
+    public static final String REGISTER = WEB_URL_RESTORAN + "register/";
+    public static final String LOGIN = WEB_URL_RESTORAN + "login/";
+}
